781c2f33dd386928c05adab16b0a28b99086b4ef
Izoska <107296018+Izoska@users.noreply.github.com>
2021-01-23 06:53:38 +0000

    Updates travis config

---
+++ backend/src/main/java/com/LJ/StockSafe/Oauth/OauthProperties.java
@@ -0,0 +1,21 @@
+package com.LJ.StockSafe.Oauth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ConfigurationProperties(prefix = "oauth2")
+public class OauthProperties {
+    private final Map<String, User> user = new HashMap<>();
+
+    private final Map<String, Provider> provider = new HashMap<>();
+
+    @Getter
+    @Setter
+    public static class User {
+        private String clientId;
+        private 
