7425208b217719b2c34392ea0c7a8c5c25319bae
Izoska <107296018+Izoska@users.noreply.github.com>
2022-12-17 19:15:04 +0000

    Style change

--- frontend/stocksafe_vue_v1/src/main.js
+++ frontend/stocksafe_vue_v1/src/main.js
@@ -1,3 +1,26 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./
+import router from "./router";
+import store from "./store";
+import { BootstrapVue, BootstrapVueIcons,  IconsPlugin } from 'bootstrap-vue'
+import Vuex from 'vuex';
+// import Chart from "chart.js";
+// import chart1 from "@/js/demo/chart-area-demo.js";
+import Dropdown from 'vue-simple-search-dropdown';
+
+import "bootstrap/dist/css/bootstrap.css";
+
+Vue.config.productionTip = false;
+// Vue.use(Chart);
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(Vuex);
+// Vue.use(chart1);
+Vue.use(Dropdown);
+Vue.use(IconsPlugin);
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ frontend/stocksafe_vue_v1/src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import LikeStock from "../views/LikeStock.vue";
+import MyStock from "../views/MyStock.vue";
+import TodayIssue from "../views/TodayIssue.vue";
+import User from "../views/User.vue";
+import StockDetail from "../views/StockDetail.vue";
+import Popup from "../views/Popup.vue";
+import UserLogin from "@/components/userinfo/UserLogin.vue";
+import UserUpdate from "@/components/userinfo/UserUpdate.vue";
+import UserJoin from "@/components/userinfo/UserJoin.vue";
+import UserPassword from "@/components/userinfo/UserPassword.vue";
+import ListStock from "../views/ListStock.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/likestock",
+    name: "LikeStock",
+    component: LikeStock,
+  },
+  {
+    path: "/mystock",
+    name: "MyStock",
+    component: MyStock,
+  },
+  {
+    path: "/list",
+    name: "ListStock",
+    component: ListStock,
+  },
+  {
+    path: "/todayissue",
+    name: "TodayIssue",
+    component: TodayIssue,
+  },
+  {
+    path: "/detail/:id",
+    name: "StockDetail",
+    component: StockDetail,
+  },
+  {
+    path: "/popup",
+    name: "Popup",
+    component: Popup,
+  },
+  {
+    path: "/member",
+    name: "User",
+    component: User,
+    children: [
+      {
+        path: "login",
+        name: "UserLogin",
+        component: UserLogin,
+      },
+      {
+        path: "update",
+        name: "UserUpdate",
