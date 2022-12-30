0df9cc326dd5d48e186a7824493eb5d6a72518ae
Izoska <107296018+Izoska@users.noreply.github.com>
2022-12-30 16:29:43 +0000

    - Implemented swipe handling on mat-drawer - Implemented uuid generation on expandjs - Improved sanitization and validation on xp-schema

--- frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
+++ frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
@@ -546,4 +546,54 @@ c
                   <div class="card-body">
                     <div class="chart-area">
                       <!-- <canvas id="myAreaChart"></canvas> -->
-    
+                      <canvas id="chart1" width="400" height="400"></canvas>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Pie Chart -->
+              <div class="col-xl-4 col-lg-5">
+                <div class="card shadow mb-4">
+                  <!-- Card Header - Dropdown -->
+                  <div
+                    class="
+                      card-header
+                      py-3
+                      d-flex
+                      flex-row
+                      align-items-center
+                      justify-content-between
+                    "
+                  >
+                    <h6 class="m-0 font-weight-bold text-primary">
+                      Revenue Sources
+                    </h6>
+                    <div class="dropdown no-arrow">
+                      <a
+                        class="dropdown-toggle"
+                        href="#"
+                        role="button"
+                        id="dropdownMenuLink"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <i
+                          class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"
+                        ></i>
+                      </a>
+                      <div
+                        class="
+                          dropdown-menu dropdown-menu-right
+                          shadow
+                          animated--fade-in
+                        "
+                        aria-labelledby="dropdownMenuLink"
+                      >
+                        <div class="dropdown-header">Dropdown Header:</div>
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"
+     
