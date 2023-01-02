eea0a4a9dafe3cf134ad2862eb3d3a47a14d64e2
Izoska <107296018+Izoska@users.noreply.github.com>
2023-01-02 10:42:29 +0000

    Spaces to tabs

--- frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
+++ frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
@@ -645,4 +645,52 @@ c
                         aria-valuemax="100"
                       ></div>
                     </div>
-                    <h4 class
+                    <h4 class="small font-weight-bold">
+                      Sales Tracking <span class="float-right">40%</span>
+                    </h4>
+                    <div class="progress mb-4">
+                      <div
+                        class="progress-bar bg-warning"
+                        role="progressbar"
+                        style="width: 40%"
+                        aria-valuenow="40"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
+                    </div>
+                    <h4 class="small font-weight-bold">
+                      Customer Database <span class="float-right">60%</span>
+                    </h4>
+                    <div class="progress mb-4">
+                      <div
+                        class="progress-bar"
+                        role="progressbar"
+                        style="width: 60%"
+                        aria-valuenow="60"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
+                    </div>
+                    <h4 class="small font-weight-bold">
+                      Payout Details <span class="float-right">80%</span>
+                    </h4>
+                    <div class="progress mb-4">
+                      <div
+                        class="progress-bar bg-info"
+                        role="progressbar"
+                        style="width: 80%"
+                        aria-valuenow="80"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
+                    </div>
+                    <h4 class="small font-weight-bold">
+                      Account Setup <span class="float-right">Complete!</span>
+                    </h4>
+                    <div class="progress">
+                      <div
+                        v-bind="key1"
+                        class="progress-bar bg-success"
+                        role="progressbar"
+                        :style="key1"
+                        aria-val
