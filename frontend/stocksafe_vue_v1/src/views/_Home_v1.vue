94ebdcabb2683d80f4804b93bd18569715eae40d
Izoska <107296018+Izoska@users.noreply.github.com>
2023-01-03 08:44:59 +0000

    add zshrc

--- frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
+++ frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
@@ -693,4 +693,52 @@ c
                         class="progress-bar bg-success"
                         role="progressbar"
                         :style="key1"
-                        aria-val
+                        aria-valuenow="100"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        {{ key1 }}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Color System -->
+                <div class="row">
+                  <div class="col-lg-6 mb-4">
+                    <div class="card bg-primary text-white shadow">
+                      <div class="card-body">
+                        Primary
+                        <div class="text-white-50 small">#4e73df</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <div class="card bg-success text-white shadow">
+                      <div class="card-body">
+                        Success
+                        <div class="text-white-50 small">#1cc88a</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <div class="card bg-info text-white shadow">
+                      <div class="card-body">
+                        Info
+                        <div class="text-white-50 small">#36b9cc</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <div class="card bg-warning text-white shadow">
+                      <div class="card-body">
+                        Warning
+                        <div class="text-white-50 small">#f6c23e</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <div class="card bg-danger text-white shadow">
+                      <div class="card-body">
+                        Danger
+             
