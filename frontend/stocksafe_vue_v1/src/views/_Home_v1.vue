f7f78bd9183321d58aa417d5491aa75b286ae4b8
Izoska <107296018+Izoska@users.noreply.github.com>
2023-01-04 17:34:46 +0000

    Feature Requests #1949622 (Mise en cache + ré-organisation des répertoires)

--- frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
+++ frontend/stocksafe_vue_v1/src/views/_Home_v1.vue
@@ -790,4 +790,58 @@ c
                       />
                     </div>
                     <p>
-                      Add some quality, svg illustrations to your projec
+                      Add some quality, svg illustrations to your project
+                      courtesy of
+                      <a
+                        target="_blank"
+                        rel="nofollow"
+                        href="https://undraw.co/"
+                        >unDraw</a
+                      >, a constantly updated collection of beautiful svg images
+                      that you can use completely free and without attribution!
+                    </p>
+                    <a target="_blank" rel="nofollow" href="https://undraw.co/"
+                      >Browse Illustrations on unDraw &rarr;</a
+                    >
+                  </div>
+                </div>
+
+                <!-- Approach -->
+                <div class="card shadow mb-4">
+                  <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">
+                      Development Approach
+                    </h6>
+                  </div>
+                  <div class="card-body">
+                    <p>
+                      SB Admin 2 makes extensive use of Bootstrap 4 utility
+                      classes in order to reduce CSS bloat and poor page
+                      performance. Custom CSS classes are used to create custom
+                      components and custom utility classes.
+                    </p>
+                    <p class="mb-0">
+                      Before working with this theme, you should become familiar
+                      with the Bootstrap framework, especially the utility
+                      classes.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.container-fluid -->
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Your Website 2021</span>
+            </div>
+          </div>
+        </footer>
+        <!-- End of Footer -->
+      </div>
+      <!
